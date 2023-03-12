3b6544818cb3e82e8f3503fb456869f2de520d90
Jan Macháček <jan.machacek@kosik.cz>
2023-03-12 20:53:32 +0100

    call ngrok

--- src/main/java/eu/personalnote/EventGridTriggerJava1.java
+++ src/main/java/eu/personalnote/EventGridTriggerJava1.java
@@ -1,6 +1,11 @@
 package eu.personalnote;
 
 import com.microsoft.azure.functions.annotation.*;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import com.microsoft.azure.functions.*;
 
 /**
@@ -9,9 +14,17 @@ import com.microsoft.azure.functions.*;
 public class EventGridTriggerJava1 {
     /**
      * This function will be invoked when an event is received from Event Grid.
+     * 
+     * @throws IOException
      */
     @FunctionName("EventGridTriggerJava1")
-    public void run(@EventGridTrigger(name = "eventGridEvent") String message, final ExecutionContext context) {
+    public void run(@EventGridTrigger(name = "eventGridEvent") String message, final ExecutionContext context)
+            throws IOException {
+
+        URL url = new URL("https://5586-77-236-214-15.ngrok.io/api/HttpExample?name=" + message);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
         context.getLogger().info("Java Event Grid trigger function executed.");
         context.getLogger().info(message);
     }
